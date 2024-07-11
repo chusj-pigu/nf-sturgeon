0e47f1cab52f5180de1b4017eeb90b7c879925e6
charlenelawdes <charlene.lawruk1996@gmail.com>
2024-07-11 09:27:56 -0400

    remove unwanted file

--- wf-run.sh
+++
@@ -1,9 +0,0 @@
-module load nextflow
-module load apptainer
-
-BAM="/project/ctb-noncodo/Platform/2024/05/PPT45/sturgeon/SIGN1048.chm13_aligned_sorted.bam"
-NAME=SIGN1048
-
-nextflow run main.nf -resume \
-    --bam $BAM \
-    --sample_id $NAME
